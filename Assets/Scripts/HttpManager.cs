0f7bbbea0b0be9e681f0c78fd4b201ac04785136
Andrés Julián Álvarez Espinosa <andresjales@gmail.com>
2022-08-21 20:52:37 -0500

    Manipulacion de token lista

--- Assets/Scripts/HttpManager.cs
+++ Assets/Scripts/HttpManager.cs
@@ -14,12 +14,37 @@ public class HttpManager : MonoBehaviour
     [SerializeField] InputField usernameField;
     [SerializeField] InputField passwordField;
 
+    private string token;
+    private string username;
+
+    private void Start()
+    {
+        token = PlayerPrefs.GetString("token");
+        username = PlayerPrefs.GetString("username");
+
+        StartCoroutine(GetPerfil());
+    }
+
     public void ClickGetScores()
     {
         StartCoroutine(GetScores());
     }
 
     public void ClickSignUp()
+    {
+        string postData = GetInputData();
+
+        StartCoroutine(SignUp(postData));
+    }
+
+    public void ClickLogIn()
+    {
+        string postData = GetInputData();
+
+        StartCoroutine(LogIn(postData));
+    }
+
+    private string GetInputData()
     {
         AuthData data = new AuthData();
 
@@ -28,7 +53,7 @@ public class HttpManager : MonoBehaviour
 
         string postData = JsonUtility.ToJson(data);
 
-        StartCoroutine(SignUp(postData));
+        return postData;
     }
 
     IEnumerator GetScores()
@@ -75,6 +100,8 @@ public class HttpManager : MonoBehaviour
             AuthData resData = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
 
             Debug.Log("Te has registrado " + resData.usuario.username);
+
+            StartCoroutine(LogIn(postData));
         }
         else
         {
@@ -85,12 +112,13 @@ public class HttpManager : MonoBehaviour
 
     IEnumerator LogIn(string postData)
     {
-        Debug.Log("LOG IN :" + postData);
         string url = URLSignUp + "/api/auth/login";
         UnityWebRequest www = UnityWebRequest.Put(url, postData);
         www.method = "POST";
         www.SetRequestHeader("content-type", "application/json");
+
         yield return www.SendWebRequest();
+
         if (www.isNetworkError)
         {
             Debug.Log("NETWORK ERROR " + www.error);
@@ -99,8 +127,9 @@ public class HttpManager : MonoBehaviour
         {
             //Debug.Log(www.downloadHandler.text);
             AuthData resData = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
-            Debug.Log("Autenticado " + resData.usuario.username + ", id:" + resData.usuario._id);
+            Debug.Log("Bienvenido " + resData.usuario.username + ", id:" + resData.usuario._id);
             Debug.Log("TOKEN: " + resData.token);
+
             PlayerPrefs.SetString("token", resData.token);
             PlayerPrefs.SetString("username", resData.usuario.username);
             SceneManager.LoadScene("Game");
@@ -111,6 +140,30 @@ public class HttpManager : MonoBehaviour
             Debug.Log(www.downloadHandler.text);
         }
     }
+
+    IEnumerator GetPerfil()
+    {
+        string url = URLSignUp + "/api/usuarios/" + username;
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.SetRequestHeader("x-token", token);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR " + www.error);
+        }
+        else if (www.responseCode == 200)
+        {
+            AuthData resData = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
+            Debug.Log("Token valido " + resData.usuario.username + ", id:" + resData.usuario._id + " y su score es: " + resData.usuario.score);
+            SceneManager.LoadScene("Game");
+        }
+        else
+        {
+            Debug.Log(www.error);
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
 }
 
 [System.Serializable]
